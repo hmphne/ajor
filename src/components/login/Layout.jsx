36e358c1100ad54a52072cbdfaf08d1f99f085a3
hmphne <3rdoperson@gmail.com>
2024-09-20 17:12:09 +0330

    feat(Layout): added login layout

---
+++ src/components/login/Layout.jsx
@@ -0,0 +1,20 @@
+import { loginSteps } from "../constants";
+import ajor_logo from '@/assets/images/ajor.webp'
+
+const Layout = ({ step, children }) => {
+  const title = loginSteps.find((i) => i.id === step).label;
+
+  return (
+    <div className="w-full px-4 py-12 flex flex-col">
+      <div className="flex justify-between items-center mb-16">
+        <span className="bg-gray-light font-pelak font-black text-2xl p-8 rounded-40">
+          {title}
+        </span>
+        <img width={80} src={ajor_logo} />
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
