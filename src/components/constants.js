668164559fade24dc19724b6edbb662ae8789791
hmphne <3rdoperson@gmail.com>
2024-09-20 17:11:20 +0330

    feat(constant): added loginSteps + relocated

---
+++ src/components/constants.js
@@ -0,0 +1,11 @@
+export const TOKEN_KEY = "ajorToken";
+export const baseRoute = "/partners/";
+
+export const loginSteps = [
+  { id: 1, label: "ورود | ثبت نام" },
+  { id: 2, label: "ورود | ثبت نام" },
+  { id: 3, label: "ورود | ثبت نام" },
+  { id: 4, label: "ثبت اطلاعات" },
+  { id: 5, label: "ثبت اطلاعات" },
+  { id: 6, label: "ثبت اطلاعات" },
+];
