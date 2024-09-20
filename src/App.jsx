200c777e8fe8fccdd77716fb85e0f87f81ccb4a8
hmphne <3rdoperson@gmail.com>
2024-09-20 17:09:38 +0330

    feat(App): display a message if user is offline

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import AuthProvider from "./hooks/authContext";
+import { useEffect, useState } from "react";
+import AuthProvider from "./hooks/authProvider";
 import Routes from "./routes";
 
 const App = () => {
-  return (
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+  useEffect(() => {
+    const handleOnline = () => setIsOnline(true);
+    const handleOffline = () => setIsOnline(false);
+
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, []);
+
+  return isOnline ? (
     <AuthProvider>
       <Routes />
     </AuthProvider>
+  ) : (
+    <div>
+      <h1>You are offline</h1>
+      <p>Please check your internet connection.</p>
+    </div>
   );
 };
 
