235e34dfb892cb5cd9cea6a54d0fb45959c414d3
hmphne <3rdoperson@gmail.com>
2024-09-21 11:07:53 +0330

    feat(utils): added formatSecondsToMinutes

---
+++ src/components/utils/utils.js
@@ -0,0 +1,7 @@
+export const formatSecondsToMinutes = (timeInSeconds) => {
+  const minutes = Math.floor(timeInSeconds / 60);
+  const seconds = timeInSeconds % 60;
+  return `${minutes < 10 ? "0" : ""}${minutes}:${
+    seconds < 10 ? "0" : ""
+  }${seconds}`;
+};
