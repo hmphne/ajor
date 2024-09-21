e36ef9cfe298c33da5229408661c3b10064a9722
hmphne <3rdoperson@gmail.com>
2024-09-21 17:14:49 +0330

    feat(Login): added last step

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,3 +1,4 @@
+import AddressStep from "@/components/login/AddressStep";
 import Layout from "@/components/login/Layout";
 import OtpStep from "@/components/login/OtpStep";
 import PhoneNumberStep from "@/components/login/PhoneNumberStep";
@@ -8,24 +9,28 @@ import usePopstate from "@/hooks/usePopstate";
 import { useContext } from "react";
 
 const Login = () => {
+  // eslint-disable-next-line no-unused-vars
   const { formData } = useContext(LoginContext);
 
   const { currentStep, setCurrentStep } = usePopstate(1);
 
   const handleNext = () => {
     const nextStep = currentStep + 1;
-    setCurrentStep(nextStep);
 
-    window.history.pushState({ step: nextStep }, `Step ${nextStep}`, "");
+    if (nextStep === 6) {
+      console.log("Login formData & Navigate to /products");
+      // TODO: make login request & navigate to /products
+    } else {
+      setCurrentStep(nextStep);
+      window.history.pushState({ step: nextStep }, `Step ${nextStep}`, "");
+    }
   };
-
-  console.log(formData);
 
   return (
     <Layout step={currentStep}>
       {currentStep === 1 && <PhoneNumberStep nextStep={handleNext} />}
       {currentStep === 2 && <OtpStep nextStep={handleNext} />}
-      {/* TODO: Check if user already exists then skip the last 3 steps */}
+      {/* TODO: Check if user already exists then skip the next 3 steps */}
       {currentStep === 3 && <UserTypeStep nextStep={handleNext} />}
       {currentStep === 4 && <UserInfoStep nextStep={handleNext} />}
       {currentStep === 5 && <AddressStep nextStep={handleNext} />}
