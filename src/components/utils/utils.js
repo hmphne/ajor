5e95fa3ef3e4e0cd0f2563e87f8255618c1a7ca7
hmphne <3rdoperson@gmail.com>
2024-09-21 18:33:21 +0330

    feat(utils): added navItems

--- src/components/utils/utils.js
+++ src/components/utils/utils.js
@@ -1,3 +1,8 @@
+import IconCart from "../icons/IconCart";
+import IconInquiry from "../icons/IconInquiry";
+import IconOrder from "../icons/IconOrder";
+import IconProducts from "../icons/IconProducts";
+
 export const formatSecondsToMinutes = (timeInSeconds) => {
   const minutes = Math.floor(timeInSeconds / 60);
   const seconds = timeInSeconds % 60;
@@ -5,3 +10,10 @@ export const formatSecondsToMinutes = (timeInSeconds) => {
     seconds < 10 ? "0" : ""
   }${seconds}`;
 };
+
+export const navItems = [
+  { label: "محصولات", to: "/products", icon: IconProducts },
+  { label: "سبد خرید", to: "/cart", icon: IconCart },
+  { label: "استعلام", to: "/inquiry", icon: IconInquiry },
+  { label: "سفارش", to: "/order", icon: IconOrder },
+];
