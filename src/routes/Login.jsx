a798f57587ba4c679e20074e3c278149d1ac4b28
hmphne <3rdoperson@gmail.com>
2024-09-21 16:44:53 +0330

    refactor(Login): refactor login removed query parameter for each step

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,25 +1,34 @@
 import Layout from "@/components/login/Layout";
 import OtpStep from "@/components/login/OtpStep";
 import PhoneNumberStep from "@/components/login/PhoneNumberStep";
+import UserInfoStep from "@/components/login/UserInfoStep";
+import UserTypeStep from "@/components/login/UserTypeStep";
 import { LoginContext } from "@/hooks/loginProvider";
+import usePopstate from "@/hooks/usePopstate";
 import { useContext } from "react";
-import useStepNavigation from "@/hooks/usePopState";
 
 const Login = () => {
   const { formData } = useContext(LoginContext);
 
-  const { step, nextStep, prevStep } = useStepNavigation(1);
+  const { currentStep, setCurrentStep } = usePopstate(1);
+
+  const handleNext = () => {
+    const nextStep = currentStep + 1;
+    setCurrentStep(nextStep);
+
+    window.history.pushState({ step: nextStep }, `Step ${nextStep}`, "");
+  };
+
+  console.log(formData);
 
   return (
-    <Layout step={step}>
-      {step === 1 && <PhoneNumberStep nextStep={nextStep} />}
-      {step === 2 && (
-        <OtpStep nextStep={nextStep} prevStep={prevStep} formData={formData} />
-      )}
-      {step === 3 && <ConfirmOtpStep nextStep={nextStep} prevStep={prevStep} />}
-      {step === 4 && <UserTypeStep nextStep={nextStep} prevStep={prevStep} />}
-      {step === 5 && <UserInfoStep nextStep={nextStep} prevStep={prevStep} />}
-      {step === 6 && <AddressStep nextStep={nextStep} prevStep={prevStep} />}
+    <Layout step={currentStep}>
+      {currentStep === 1 && <PhoneNumberStep nextStep={handleNext} />}
+      {currentStep === 2 && <OtpStep nextStep={handleNext} />}
+      {/* TODO: Check if user already exists then skip the last 3 steps */}
+      {currentStep === 3 && <UserTypeStep nextStep={handleNext} />}
+      {currentStep === 4 && <UserInfoStep nextStep={handleNext} />}
+      {currentStep === 5 && <AddressStep nextStep={handleNext} />}
     </Layout>
   );
 };
