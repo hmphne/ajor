4625bf30c13f13a09cdbf771513020b5e4ed7271
hmphne <3rdoperson@gmail.com>
2024-09-21 11:07:41 +0330

    feat(Input): added direction

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -8,7 +8,7 @@ const Input = React.forwardRef(({ className, type, beforeIcon, afterIcon, ...pro
       <input
         type={type}
         className={cn(
-          "flex w-full border-0 bg-transparent p-3 text-sm shadow-sm transition-colors placeholder:text-gray-dark focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+          "flex w-full border-0 bg-transparent p-3 text-sm shadow-sm transition-colors placeholder:text-right placeholder:text-gray-dark focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
