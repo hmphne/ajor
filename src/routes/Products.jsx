9959b5f0ad98da9e41fd3b0f33cdef813d0a8301
hmphne <3rdoperson@gmail.com>
2024-09-21 18:36:52 +0330

    refactor(Products): renamed to products

---
+++ src/routes/Products.jsx
@@ -0,0 +1,18 @@
+const Products = () => {
+  return (
+    <div>
+      hi Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem laborum
+      qui commodi culpa pariatur consectetur tenetur incidunt unde nostrum quo
+      tempora ab sed natus fugiat! Temporibus animi vero dicta dolore ipsam cum.
+      soluta quae perspiciatis voluptatum fuga nulla. Pariatur voluptas unde
+      ratione unde, eligendi est cum minima. Nulla maiores sint voluptatibus
+      commodi totam minima suscipit. Quia rerum expedita autem totam sunt
+      aliquam similique quos sequi soluta corrupti veritatis, nemo natus? Di
+      atque cupiditate assumenda eius. Rerum, repellendus. Quas quia minus
+      corporis! Quibusdam quidem officiis adipisci modi pariatur similique
+      eligend
+    </div>
+  );
+};
+
+export default Products;
--- src/routes/Protected.jsx
+++
@@ -1,6 +0,0 @@
-
-const Protected = () => {
-  return <div className="flex flex-col w-full relative">PROTECTED</div>;
-};
-
-export default Protected;
