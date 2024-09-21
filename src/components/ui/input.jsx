21c8f00beb5c4064d22692786b67c790aed72f56
hmphne <3rdoperson@gmail.com>
2024-09-21 16:47:52 +0330

    style(Input): fixed styles

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -1,24 +1,33 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
-const Input = React.forwardRef(({ className, type, beforeIcon, afterIcon, ...props }, ref) => {
-  return (
-    <div className="flex items-center border border-input rounded-lg bg-transparent h-[50px]">
-      {beforeIcon && <div className="pr-3">{beforeIcon}</div>}
-      <input
-        type={type}
-        className={cn(
-          "flex w-full border-0 bg-transparent p-3 text-sm shadow-sm transition-colors placeholder:text-right placeholder:text-gray-dark focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-          className
+const Input = React.forwardRef(
+  ({ className, type, beforeIcon, afterIcon, ...props }, ref) => {
+    return (
+      <div className="flex items-center border border-input rounded-lg bg-transparent h-[50px]">
+        {beforeIcon && <div className="pr-3">{beforeIcon}</div>}
+        <input
+          type={type}
+          className={cn(
+            "flex w-full border-0 bg-transparent p-3 text-sm transition-colors placeholder:text-right placeholder:text-gray-dark focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+            className
+          )}
+          ref={ref}
+          autoFocus
+          {...props}
+        />
+        {afterIcon && (
+          <div className="border-l border-gray-300 h-[42px] py-1 mr-2 ml-4" />
         )}
-        ref={ref}
-        {...props}
-      />
-      {afterIcon && <div className="border-l border-gray-300 h-[42px] py-1 mr-2 ml-4" />}
-      {afterIcon && <div className="pl-3 text-gray">{afterIcon}</div>}
-    </div>
-  );
-});
+        {afterIcon && (
+          <div dir="ltr" className="pl-3 text-gray">
+            {afterIcon}
+          </div>
+        )}
+      </div>
+    );
+  }
+);
 Input.displayName = "Input";
 
 export { Input };
