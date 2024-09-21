6a4c8db70305f13b4c228dc88a71ce188db54f89
hmphne <3rdoperson@gmail.com>
2024-09-21 16:48:54 +0330

    fix(constants): removed one step

--- src/components/utils/constants.js
+++ src/components/utils/constants.js
@@ -8,8 +8,7 @@ export const initialTimer = 10;
 export const loginSteps = [
   { id: 1, label: "ورود | ثبت نام" },
   { id: 2, label: "ورود | ثبت نام" },
-  { id: 3, label: "ورود | ثبت نام" },
+  { id: 3, label: "ثبت اطلاعات" },
   { id: 4, label: "ثبت اطلاعات" },
   { id: 5, label: "ثبت اطلاعات" },
-  { id: 6, label: "ثبت اطلاعات" },
 ];
